7e155bacb90b8ae69d85e8109b38ef4ae9970d0f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-04 11:47:48 +0700

    menambahkan penyedia view model

---
+++ app/src/main/java/com/example/tugasakhirpam/view/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.tugasakhirpam.viewmodel.provider
+
+import android.content.Context
+import com.example.tugasakhirpam.data.database.MovieDatabase
+import com.example.tugasakhirpam.data.repository.FilmRepository
+import com.example.tugasakhirpam.data.repository.ReportRepository
+import com.example.tugasakhirpam.data.repository.UserRepository
+import com.example.tugasakhirpam.view.viewmodel.ReportViewModel
+import com.example.tugasakhirpam.viewmodel.AuthViewModel
+import com.example.tugasakhirpam.viewmodel.FilmViewModel
+
+object PenyediaViewModel {
+
+    fun provideAuthViewModel(context: Context): AuthViewModel {
+        val db = MovieDatabase.getDatabase(context)
+        return AuthViewModel(UserRepository(db.userDao()))
+    }
+
+    fun provideFilmViewModel(context: Context): FilmViewModel {
+        val db = MovieDatabase.getDatabase(context)
+        return FilmViewModel(FilmRepository(db.filmDao()))
+    }
+
+    fun provideReportViewModel(context: Context): ReportViewModel {
+        val db = MovieDatabase.getDatabase(context)
+        return ReportViewModel(ReportRepository(db.reportDao()))
+    }
+
+    fun provideFilmRepository(context: Context): FilmRepository {
+        val db = MovieDatabase.getDatabase(context)
+        return FilmRepository(db.filmDao())
+    }
+}
