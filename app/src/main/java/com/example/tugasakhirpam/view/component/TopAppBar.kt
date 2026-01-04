827523e3c38ca281f55ee530012cea7daafc7cdd
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-04 11:51:00 +0700

    menambahkan top app bar

---
+++ app/src/main/java/com/example/tugasakhirpam/view/component/TopAppBar.kt
@@ -0,0 +1,43 @@
+package com.example.tugasakhirpam.view.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(
+    title: String,
+    onBack: (() -> Unit)? = null,
+    onLogout: (() -> Unit)? = null
+) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            if (onBack != null) {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        },
+        actions = {
+            if (onLogout != null) {
+                IconButton(onClick = onLogout) {
+                    Icon(
+                        imageVector = Icons.Default.ExitToApp,
+                        contentDescription = "Logout"
+                    )
+                }
+            }
+        }
+    )
+}
