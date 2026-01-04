4fdc292af2d0460bcde702ed4aca6150501d9e7a
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-04 11:51:31 +0700

    menambahkan film card

---
+++ app/src/main/java/com/example/tugasakhirpam/view/component/FilmCard.kt
@@ -0,0 +1,48 @@
+package com.example.tugasakhirpam.view.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.tugasakhirpam.data.model.Film
+
+@Composable
+fun FilmCard(
+    film: Film,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(onClick = onClick),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column {
+            AsyncImage(
+                model = film.poster,
+                contentDescription = film.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(film.title, fontWeight = FontWeight.Bold)
+                Text("Genre: ${film.genre}")
+                Text("Rating: ${film.rating}")
+            }
+        }
+    }
+}
